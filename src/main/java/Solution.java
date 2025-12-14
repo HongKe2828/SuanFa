3a7e3cea353f31068eef691358499ab7b1e3cf86
hanzhijia <2399291570@qq.com>
2025-12-14 17:01:19 +0800

    删了while

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -3,31 +3,12 @@ import java.util.Arrays;
 class Solution {
 
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-
-
         int p1 = 0;
         int p2 = 0;
         int p = 0;
         int[] nums1_copy = new int[n + m];
         System.arraycopy(nums1, 0, nums1_copy, 0, m);
-        while (p1 < m || p2 < n) {
-            if(p1==m){
-                nums1[p]=nums2[p2++];
 
-            }
-            else if(p2==n){
-                nums1[p]=nums1_copy[p1++];
-
-            }
-            else if (nums1_copy[p1] <= nums2[p2]) {
-                nums1[p] = nums1_copy[p1++];
-
-            } else {
-                nums1[p] = nums2[p2++];
-
-            }
-            p++;
-        }
 
     }
 
